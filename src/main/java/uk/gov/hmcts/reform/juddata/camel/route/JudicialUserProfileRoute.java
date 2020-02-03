ae072aa60af4aec8711efa457baa3d0fa0438757
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-02-03 17:21:05 +0000

    RDCC-896 Added office authorization route

--- src/main/java/uk/gov/hmcts/reform/juddata/camel/route/JudicialUserProfileRoute.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/camel/route/JudicialUserProfileRoute.java
@@ -4,10 +4,13 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.BindyType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.juddata.camel.beans.JudicialOfficeAuthorisation;
 import uk.gov.hmcts.reform.juddata.camel.beans.JudicialUserProfile;
 import uk.gov.hmcts.reform.juddata.camel.mapper.JudicialOfficeAppointmentRowMapper;
 import uk.gov.hmcts.reform.juddata.camel.mapper.JudicialOfficeAuthorisationRowMapper;
 import uk.gov.hmcts.reform.juddata.camel.mapper.JudicialUserRowMapper;
+import uk.gov.hmcts.reform.juddata.camel.processor.JudicialOfficeAuthorisationFileReadProcessor;
+import uk.gov.hmcts.reform.juddata.camel.processor.JudicialOfficeAuthorisationProcessor;
 import uk.gov.hmcts.reform.juddata.camel.processor.JudicialUserProfileFileReadProcessor;
 import uk.gov.hmcts.reform.juddata.camel.processor.JudicialUserProfileProcessor;
 
@@ -18,10 +21,8 @@ public class JudicialUserProfileRoute extends RouteBuilder {
     JudicialUserRowMapper judUserRowMapper;
 
     @Autowired
-    JudicialOfficeAppointmentRowMapper judicialOfficeAppointmentRowMapper;
-
-    @Autowired
     JudicialOfficeAuthorisationRowMapper judicialOfficeAuthorisationRowMapper;
+
 
     @Override
     public void configure() {
@@ -41,6 +42,17 @@ public class JudicialUserProfileRoute extends RouteBuilder {
                         + " contract_type = :#contract_type, work_pattern = :#work_pattern, email_id = :#email_id, joining_date = :#joining_date, "
                         + "last_working_date = :#last_working_date, active_flag = :#active_flag, "
                         + "extracted_date = :#extracted_date, last_loaded_date = now() at time zone 'utc'?dataSource=dataSource")
+                .to("log:test?showAll=true")
+                .end()
+                .process(new JudicialOfficeAuthorisationFileReadProcessor()).unmarshal().bindy(BindyType.Csv, JudicialOfficeAuthorisation.class)
+                .process(new JudicialOfficeAuthorisationProcessor())
+                .split().body()
+                .bean(judicialOfficeAuthorisationRowMapper, "getMap")
+                //.to("sql:insert into authorisation_type (authorisation_id,authorisation_desc_en,authorisation_desc_cy,jurisdiction_id,jurisdiction_desc_en,jurisdiction_desc_cy) values ('1', 'Voluntary_en','Voluntary_cy','1','jud_desc_ec','jud_desc_cy')?dataSource=dataSource")
+                //.to("sql:insert into authorisation_type (authorisation_id,authorisation_desc_en,authorisation_desc_cy,jurisdiction_id,jurisdiction_desc_en,jurisdiction_desc_cy) values ('2', 'Voluntary_en','Voluntary_cy','2','jud_desc_ec','jud_desc_cy')?dataSource=dataSource")
+                .to("sql:truncate judicial_office_authorisation?dataSource=dataSource")
+                .to("sql:insert into judicial_office_authorisation (judicial_office_auth_id,elinks_id,authorisation_id,jurisdiction_id,authorisation_date,extracted_date,created_date,last_loaded_date) "
+                        + "values(:#judicial_office_auth_id,:#elinks_id,:#authorisation_id, :#jurisdiction_id,:#authorisation_date,:#extracted_date,:#created_date,:#last_loaded_date)?dataSource=dataSource")
                 .to("log:test?showAll=true")
                 .end();
     }
